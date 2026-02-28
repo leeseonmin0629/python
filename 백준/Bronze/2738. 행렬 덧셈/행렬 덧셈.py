9bc6f21fb4f8f92abee071740ce0900aef131a63
leeseonmin0629 <seonminolivia@gmail.com>
2026-02-28 18:12:15 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 48 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.py
@@ -1,16 +1,17 @@
 n, m = map(int, input().split())
-lstA = []
-lstB = []
+matrix1 = []
+matrix2 = []
+matrix = []
 for i in range(n):
-    lstA.append(list(map(int, input().split())))
+    temp = list(map(int, input().split()))
+    matrix1.append(temp)
 for i in range(n):
-    lstB.append(list(map(int, input().split())))
-finalList = []
-for i in range(len(lstA)):
-    accumulatedList = []
-    for j in range(len(lstA[i])):
-        accumulatedList.append(lstA[i][j] + lstB[i][j])
-    finalList.append(accumulatedList)
-
-for arr in finalList:
-    print(*arr)
+    temp = list(map(int, input().split()))
+    matrix2.append(temp)
+for i in range(n):
+    temp1 = []
+    for j in range(m):
+        temp1.append(int(matrix1[i][j]) + int(matrix2[i][j]))
+    matrix.append(temp1)
+for i in range(n):
+    print(*matrix[i])
